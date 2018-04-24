8724946371da19455d92e7a77d9e0065d529ddfe
xunanhang@xiaoniu66@com <xn060263@xiaoniu66.com>
2018-04-24 10:47:15 +0800

    add cust.html && hot.html

--- js/comm.js
+++ js/comm.js
@@ -1,20 +1,19 @@
-$(function(){
-//		$("#J_headerCategory").mouseover(function(){
-//			$(this).find(".nav-left-tab").slideDown()
-//		},mouseout(function(){
-//			$(this).find(".nav-left-tab").slideUp();
-//		}));
-		
-		$("#J_headerCategory").mouseover(function(){
-			$(this).find(".nav-left-tab").slideDown();
-		});
-		$("#J_headerCategory").mouseout(function(){
-			$(this).find(".nav-left-tab").slideUp();
-		});
-		
-		
-	$(".clearfix li a.link").click(function(){
-		$(this).addClass("selected").siblings("a").removeClass("selected");
+$(function() {
+	$("#J_headerCategory").mouseover(function() {
+		$(this).find(".nav-left-tab").stop().slideDown();
 	});
-	
+	$("#J_headerCategory").mouseout(function() {
+		$(this).find(".nav-left-tab").stop().slideUp();
+	});
+
+	$(".clearfix li a.link").click(function() {
+		$(this).addClass("on").siblings("a").removeClass("on");
+	});
+
+});
+
+$(document).on("click", "#J_brandbtn a", function(){
+	var _val = $(this).attr("data-val");
+	$(this).addClass("selected-on").parent().siblings("li").find("a").removeClass("selected-on");
+	0 != _val ? ($("#J_brandlist li").hide(), $('#J_brandlist a[data-alpha="' + _val + '"]').parent().show()) : $("#J_brandlist li").show()
 });
